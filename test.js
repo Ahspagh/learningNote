9250482bdcd35ac2204ca032f1b21160c3211455
lrfeng <ruifeng@jyquant.com.cn>
2021-11-29 11:38:56 +0800

    2021-11-29

--- test.js
+++ test.js
@@ -380,3 +380,22 @@ var hammingDistance2 = function (x, y) {
 
 //       git fetch origin --prune-tags
 // 5.git pull 拉取代码
+
+
+// 解决刷新后vuex丢失store的解决方法
+// created() {
+//     //在页面加载时读取sessionStorage里的状态信息
+//     if (sessionStorage.getItem("store")) {
+//       this.$store.replaceState(
+//         Object.assign(
+//           {},
+//           this.$store.state,
+//           JSON.parse(sessionStorage.getItem("store"))
+//         )
+//       );
+//     }
+//     //在页面刷新时将vuex里的信息保存到sessionStorage里
+//     window.addEventListener("beforeunload", () => {
+//       sessionStorage.setItem("store", JSON.stringify(this.$store.state));
+//     });
+// },
